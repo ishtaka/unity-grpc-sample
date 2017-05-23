5fe545bb0e55eda1dc2ff815aab58ab2c2a72548
ahakits <ahakits1987@gmail.com>
2017-05-23 23:35:51 +0900

    Add gRPC sample

---
+++ Assets/Scripts/Client/GreeterClient.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using Helloworld;
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GreeterClient : MonoBehaviour
+{
+    [SerializeField]
+    Button button;
+
+    void Start()
+    {
+        button.OnClickAsObservable().Subscribe(_ => Send()).AddTo(this);
+    }
+
+    void Send()
+    {
+        Channel channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+
+        var client = new Greeter.GreeterClient(channel);
+        String user = "you";
+
+        HelloReply reply = client.SayHello(new HelloRequest { Name = user });
+        Debug.Log("Greeting: " + reply.Message);
+
+        channel.ShutdownAsync();
+    }
+}
---
+++ Assets/Scripts/Server/GreeterServer.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Helloworld;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GreeterServer : MonoBehaviour
+{
+    class GreeterImpl : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+    }
+
+    const int Port = 50051;
+
+    void Start()
+    {
+        Server server = new Server
+        {
+            Services = { Greeter.BindService(new GreeterImpl()) },
+            Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+        };
+
+        server.Start();
+    }
+}
