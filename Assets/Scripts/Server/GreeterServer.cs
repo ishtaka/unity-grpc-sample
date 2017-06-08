c3e12e53dfb45581e4046ed72a32365cfaab2abc
ahakits <ahakits1987@gmail.com>
2017-06-09 03:46:08 +0900

    Add lanch server button & channel button

--- Assets/Scripts/Server/GreeterServer.cs
+++ Assets/Scripts/Server/GreeterServer.cs
@@ -2,9 +2,17 @@
 using Helloworld;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
 public class GreeterServer : MonoBehaviour
 {
+    [SerializeField]
+    Text output;
+
+    [SerializeField]
+    Button button;
+
     class GreeterImpl : Greeter.GreeterBase
     {
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
@@ -17,12 +25,27 @@ public class GreeterServer : MonoBehaviour
 
     void Start()
     {
-        Server server = new Server
-        {
-            Services = { Greeter.BindService(new GreeterImpl()) },
-            Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
-        };
+        button.OnClickAsObservable()
+            .Subscribe(_ => StartServer())
+            .AddTo(this);
+    }
 
-        server.Start();
+    void StartServer()
+    {
+        try
+        {
+            Server server = new Server
+            {
+                Services = { Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("0.0.0.0", Port, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+            output.text = "Start Server";
+        }
+        catch (System.Exception e)
+        {
+            output.text = e.Message;
+        }
     }
 }
